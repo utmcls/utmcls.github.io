1a401d5a43eac56aff480fab675f9ea22196029e
Nick Chen <nickchen3889@gmail.com>
2026-01-15 20:49:39 -0500

    feat: initial setup ready to deploy to vercel

---
+++ keystatic.config.ts
@@ -0,0 +1,18 @@
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'github',
+    repo: {
+      owner: 'utmcls',
+      name: 'utmcls.github.io',
+    },
+  },
+  collections: {
+    // Future content types like:
+    // - pages
+    // - blog posts
+    // - events
+    // - team members
+  },
+});
